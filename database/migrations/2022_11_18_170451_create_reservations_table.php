82d28e3b402519dde21d12cfba60e7dc9cd3dde9
renehuanca <renehuanca999@gmail.com>
2022-11-18 13:30:37 -0400

    migrations have been created

---
+++ database/migrations/2022_11_18_170451_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('email');
+			$table->string('phone');
+			$table->datetime('to_date');
+			$table->foreignIdFor(Table::class);
+			$table->integer('guest_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
---
+++ database/migrations/2022_11_18_171315_create_category_menu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
+use App\Models\Menu;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_menu', function (Blueprint $table) {
+            $table->id();
+			$table->foreignIdFor(Category::class);
+			$table->foreignIdFor(Menu::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_menu');
+    }
+};
