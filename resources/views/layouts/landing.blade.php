002b82f0b24d7cd42645e2dbaf5e06e33b77f91d
Rene Huanca <renehuanca999@gmail.com>
2023-02-28 18:48:28 -0400

    feat: home section and menu dark

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -15,9 +15,8 @@
 </head>
 
 <body class="antialiased">
-
-  <!-- This example requires Tailwind CSS v3.0+ -->
-  <div class="isolate bg-white">
+  <div class="isolate bg-white dark:bg-gray-900 text-gray-600 dark:text-gray-300 bg-dots-dark">
+    <!-- Bg Gradient -->
     <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
       <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
@@ -29,8 +28,11 @@
         </defs>
       </svg>
     </div>
+
+    <!-- Menu -->
     <div class="px-6 py-6 lg:px-8">
       <div x-data="{open: false}">
+        <!-- Desktop menu -->
         <nav class="flex h-9 items-center justify-between" aria-label="Global">
           <div class="flex lg:min-w-0 lg:flex-1" aria-label="Global">
             <a href="{{ route('welcome') }}" class="-m-1.5 p-1.5">
@@ -39,7 +41,7 @@
             </a>
           </div>
           <div class="flex lg:hidden">
-            <button @click="open = true" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <button @click="open = true" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-300">
               <span class="sr-only">Open main menu</span>
               <!-- Heroicon name: outline/bars-3 -->
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -48,30 +50,28 @@
             </button>
           </div>
           <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
-            <a href="{{ route('welcome') }}" class="font-semibold text-gray-900 hover:text-gray-900">Home</a>
-
-            <a href="#categories" class="font-semibold text-gray-900 hover:text-gray-900">Categories</a>
-
-            <a href="#ourMenu" class="font-semibold text-gray-900 hover:text-gray-900">Menu</a>
-
-            <a href="#makeReservation" class="font-semibold text-gray-900 hover:text-gray-900">Reservation</a>
+            <a href="{{ route('welcome') }}" class="font-semibold text-gray-900 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100">Home</a>
+            <a href="#categories" class="font-semibold text-gray-900 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100">Categories</a>
+            <a href="#ourMenu" class="font-semibold text-gray-900 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100">Menu</a>
           </div>
-          @if (Route::has('login') && false)
+          @if (Route::has('login'))
           <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
             @auth
-            <a href="{{ url('/dashboard') }}" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Dashboard</a>
+            <a href="{{ url('/dashboard') }}" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:hover:text-gray-100">Dashboard</a>
             @else
-            <a href="{{ route('login') }}" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Log in</a>
+            <a href="{{ route('login') }}" class="inline-block rounded-lg px-3 py-1.5 text-sm dark:text-gray-300 font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20dark:hover:text-gray-100">Log in</a>
             @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20 mx-2">Register</a>
+            <a href="{{ route('register') }}" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold  dark:text-gray-300 leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20 mx-2dark:hover:text-gray-100">Register</a>
             @endif
             @endauth
           </div>
           @endif
         </nav>
+        <!-- End Desktop menu -->
+
         <!-- Mobile menu, show/hide based on menu open state. -->
         <div x-show="open" role="dialog" aria-modal="true">
-          <div focus="true" class="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+          <div focus="true" class="fixed inset-0 z-10 overflow-y-auto bg-white dark:bg-gray-900 px-6 py-6 lg:hidden">
             <div class="flex h-9 items-center justify-between">
               <div class="flex">
                 <a href="#" class="-m-1.5 p-1.5">
@@ -80,7 +80,7 @@
                 </a>
               </div>
               <div class="flex">
-                <button @click="open = false" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <button @click="open = false" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-300">
                   <span class="sr-only">Close menu</span>
                   <!-- Heroicon name: outline/x-mark -->
                   <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -90,25 +90,21 @@
               </div>
             </div>
             <div class="mt-6 flow-root">
-              <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="-my-6 divide-y divide-gray-500/10 dark:divide-gray-500">
                 <div class="space-y-2 py-6">
-                  <a href="{{ route('welcome') }}" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Home</a>
-
-                  <a href="#categories" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Categories</a>
-
-                  <a href="#ourMenu" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Menu</a>
-
-                  <a href="#makeReservation" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">Reservation</a>
+                  <a href="{{ route('welcome') }}" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 hover:bg-gray-400/10">Home</a>
+                  <a href="#categories" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 hover:bg-gray-400/10">Categories</a>
+                  <a href="#ourMenu" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 hover:bg-gray-400/10">Menu</a>
                 </div>
 
                 <div class="py-6">
-                  @if (Route::has('login') && false)
+                  @if (Route::has('login'))
                   @auth
-                  <a href="{{ url('/dashboard') }}" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Dashboard</a>
+                  <a href="{{ url('/dashboard') }}" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 dark:text-gray-300 hover:bg-gray-400/10">Dashboard</a>
                   @else
-                  <a href="{{ route('login') }}" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Log in</a>
+                  <a href="{{ route('login') }}" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 dark:text-gray-300 hover:bg-gray-400/10">Log in</a>
                   @if (Route::has('register'))
-                  <a href="{{ route('register') }}" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Register</a>
+                  <a href="{{ route('register') }}" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 dark:text-gray-300 hover:bg-gray-400/10">Register</a>
                   @endif
                   @endauth
                   @endif
@@ -117,12 +113,13 @@
             </div>
           </div>
         </div>
+        <!-- End mobile menu -->
       </div>
     </div>
+    <!-- End menu -->
     <main>
       {{ $slot }}
     </main>
   </div>
 </body>
-
 </html>
