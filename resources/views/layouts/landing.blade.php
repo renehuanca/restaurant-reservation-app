0df2e05af2abcfe823a57b8a2c8eacb6ac8d4414
Rene Huanca <renehuanca999@gmail.com>
2023-03-01 11:30:47 -0400

    fix: inline-block is the same than flex

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -64,7 +64,7 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
           </div>
 
           <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
-            <div class="inline-block flex items-center text-gray-600 dark:text-gray-300 mr-2" @click="isDark = !isDark; if(isDark) {localStorage.theme = 'dark'} else {localStorage.theme = 'light'}">
+            <div class="flex items-center text-gray-600 dark:text-gray-300 mr-2" @click="isDark = !isDark; if(isDark) {localStorage.theme = 'dark'} else {localStorage.theme = 'light'}">
               <svg x-show="!isDark" x-transition xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
               </svg>
