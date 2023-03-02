200d54e0bb46c4f160e2665e4e665faf9ac9e913
Rene Huanca <renehuanca999@gmail.com>
2023-03-01 20:28:32 -0400

    feat: change the reservation create because in dev

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -31,7 +31,7 @@
 							<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 								<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
 							</svg>
-							<span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Reservations List</span>
+							<span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">List</span>
 						</div>
 					</li>
 				</ol>
@@ -42,7 +42,7 @@
 					<!-- component -->
 					<div class="">
 						<div class="flex flex-col sm:flex-row items-center justify-between pb-4">
-							<a href="{{ route('reservations.create') }}" class="space-x-8">
+							<a href="{{ route('reservations.step_one') }}" class="space-x-8">
 								<button class="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">{{ __('New Reservation') }}</button>
 							</a>
 							<div class="flex items-center p-2 gap-2 rounded-md">
